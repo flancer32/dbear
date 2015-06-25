1c761e6b733621ad18336487d85babd8c32ef4aa
Alex Gusev <flancer64@gmail.com>
2015-06-25 16:58:21 +0300

    WF-18 DED Sample 03: Email Authentication
    WF-18 DED Sample 03: Email Authentication

--- sample/01_person/sample_01.sql
+++ sample/01_person/sample_01.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS person;
-CREATE TABLE person (
+DROP TABLE IF EXISTS e_person;
+CREATE TABLE e_person (
   id int UNSIGNED NOT NULL AUTO_INCREMENT,
   NameFirst varchar(255) NOT NULL,
   NameLast varchar(255) NOT NULL,
