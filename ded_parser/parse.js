567bd7633a5c950696240a5417db5d236370f10e
Alexandr-Baluhin <alexbalwork@gmail.com>
2015-07-07 15:52:53 +0300

    Improve XML-parser

--- ded_parser/parse.js
+++ ded_parser/parse.js
@@ -1,24 +1,27 @@
-var fs = require('fs')
-var xml2js = require('xml2js')
-//var parser = xml2js.Parser({ explicitArray: true });
-var xml;
-var prefixMatch;
+var fs = require('fs') // fs instance
+var xml2js = require('xml2js') // xml2js instance
+var xpath = require('xml2js-xpath') //xpath instance (currently not used)
 
-prefixMatch = new RegExp(/(?!xmlns)^.*:/);
+var prefixMatch = new RegExp(/(?!xmlns)^.*:/);
 
 function tagStripPrefix(name){
-    return name.replace(prefixMatch, '');
+    return name.replace(prefixMatch, ''); //function to strip tag prefix
 }
 
 fs.readFile('ded.xml', 'utf8', function (err, data) {
     if (err) throw err
     xml2js.parseString(data, {
-        tagNameProcessors: [tagStripPrefix],},
+        tagNameProcessors: [tagStripPrefix], // strip tag prefix
+        explicitArray: false, // remove arrays in child nodes
+        mergeAttrs: true}, // attributes become child nodes
         function (err, result) {
         var str = JSON.stringify(result, function(key, value) {
-            if (key == '$') return undefined;
+            if (key == 'xmlns:tns') return undefined; // remove xmlns:tns tag
+            if (key == 'xmlns:xsi') return undefined; // remove xmlns:xsi tag
+            if (key == 'xsi:schemaLocation') return undefined; // remove xsi:schemaLocation tag
             return value;
         }, 4)
+
         fs.writeFile('ded.json', str)
     })
 })
