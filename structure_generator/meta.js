9a176957dd719e72da262903a5614746ea327220
Viktor <viktor@nmmlm.com>
2015-07-07 18:54:30 +0300

    WF-27 Refactoring

--- structure_generator/meta.js
+++ structure_generator/meta.js
@@ -21,28 +21,6 @@ function setConnection(db_name, login, password) {
     return result;
 }
 
-function createEntities(entities) {
-    for (var i = 0; i < entities.length; i++) {
-        console.log(JSON.stringify(entities[i].attributes.blue));
-        db_entities[i] = sequelize.define(entities[0].id, entities[i].attributes);
-        /*TODO change this void according to new JSON structure.*/
-    }
-}
-
-function getNamespaces(request) {
-    var result = [];
-    for (var i = 0; i < request.dBEAR.namespaces.length; i++) {
-        result[i] = request.dBEAR.namespaces[i];
-        console.log(result[i].id + ' - Namespace');
-        console.log(result[i].comment + ' - Comment');
-        for (var j = 0; j < result[i].entities.length; j++) {
-            console.log(JSON.stringify(result[i].entities[j].attributes));
-            db_entities[j] = sequelize.define(result[i].entities[j].id, result[i].entities[j].attributes)
-        }
-    }
-    return result;
-}
-
 function createMeta(JSON) {
     // CreateMetaInstances
     var meta_n = sequelize.define('_n', {
