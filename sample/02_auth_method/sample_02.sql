4cb7c451bcfb94addd8e81f76284e068ee967b3a
Alex Gusev <flancer64@gmail.com>
2015-07-09 12:10:01 +0300

    WF-30 - Improve Sample 02

--- sample/02_auth_method/sample_02.sql
+++ sample/02_auth_method/sample_02.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS r_person_authtype;
-DROP TABLE IF EXISTS e_authtype;
+DROP TABLE IF EXISTS r_person_authmethod;
+DROP TABLE IF EXISTS e_authmethod;
 DROP TABLE IF EXISTS e_person;
 
-CREATE TABLE e_authtype (
+CREATE TABLE e_authmethod (
   id int UNSIGNED NOT NULL AUTO_INCREMENT,
   Value varchar(255) NOT NULL,
   PRIMARY KEY (id)
@@ -15,13 +15,13 @@ CREATE TABLE e_person (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE r_person_authtype (
+CREATE TABLE r_person_authmethod (
   id int UNSIGNED NOT NULL AUTO_INCREMENT,
   person int UNSIGNED DEFAULT NULL,
-  authtype int UNSIGNED NOT NULL,
+  authmethod int UNSIGNED NOT NULL,
   PRIMARY KEY (id),
-  CONSTRAINT FK_person_authtype_person_id FOREIGN KEY (person)
+  CONSTRAINT FK_person_authmethod_person_id FOREIGN KEY (person)
   REFERENCES e_person (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT FK_person_authtype_authtype_id FOREIGN KEY (authtype)
-  REFERENCES e_authtype (id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT FK_person_authmethod_authmethod_id FOREIGN KEY (authmethod)
+  REFERENCES e_authmethod (id) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
