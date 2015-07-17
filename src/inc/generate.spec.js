63956cd5a91152e6151ae0a222b428b6ffdf2610
Viktor <viktor@nmmlm.com>
2015-07-17 12:33:56 +0300

    Changed tests

--- src/inc/generate.spec.js
+++ src/inc/generate.spec.js
@@ -1,15 +1,20 @@
 /* Define dependencies */
 //var request = require('../../sample/01_person/sample_01.dem.json');
 var Generator = require('./generate');
-var assert = require('chai')
+var assert = require('chai');
+var Params = require('./generate/params');
 
 describe('Generator module', function () {
     describe('#setConnection()', function () {
         var sg = new Generator();
+        var params = new Params;
+        params.dbLogin = 'sample';
+        params.dbName = 'sample_sequelize';
+        params.dbPass = '3Jcftix7VycNkEYKxIDW';
         // 1st test
         it('should authenticate with correct data', function (done) {
-            sg.setConnection('sample_sequelize', 'sample', '3Jcftix7VycNkEYKxIDW').then(function (resolve) {
-                assert.isTrue(true, 'Connection test1 succeed')
+            sg.setConnection(params).then(function (resolve) {
+                assert.isTrue(true, 'Connection test1 succeed');
                 done()
             }, function (reject) {
                 assert.isTrue(false, 'Connection test1 failed \n' + reject)
@@ -17,8 +22,9 @@ describe('Generator module', function () {
             });
         });
         // 2nd test
+        //params.dbLogin = 'wrong_login';
         it('should not authenticate with incorrect data', function () {
-            sg.setConnection('sample_sequelize', 'sample', '3Jcftix7VycNkEYKxIDW').then(function (resolve) {
+            sg.setConnection(params).then(function (resolve) {
                 assert.isTrue(false, 'Connection test2 failed \n Error was expected.')
             }, function (reject) {
                 assert.isTrue(true, 'Connection test2 succeed')
