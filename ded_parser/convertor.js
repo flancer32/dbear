8084c1a7c5c940f652e5e4bcb927ad58c9026ebe
Alexandr-Baluhin <alexbalwork@gmail.com>
2015-07-17 17:34:26 +0300

    Convertor update

--- ded_parser/convertor.js
+++ ded_parser/convertor.js
@@ -3,21 +3,23 @@ var xml2js = require('xml2js') // xml2js instance
 
 var prefixMatch = new RegExp(/(?!xmlns)^.*:/);
 
+module.exports = fs
+
 function tagStripPrefix(name){
     var result = name.replace(prefixMatch, '');
     return result //function to strip tag prefix
 }
 
-fs.readFile('ded.xml', 'ascii', function (err, data) {
+fs.readFile('sample_02.dem.xml', 'ascii', function (err, data) {
     if (err) throw err
     xml2js.parseString(data, {
         tagNameProcessors: [tagStripPrefix], // strip tag prefix
         explicitArray: false, // remove arrays in child nodes
         mergeAttrs: true}, // attributes become child nodes
         function (err, result) {
-        var str = JSON.stringify(result, function(key, value) {
+        var str = JSON.stringify(result, function (key, value) {
             var result = value;
-            if (key == 'xmlns:tns' || key == 'xmlns:xsi' || key == 'xsi:schemaLocation') result = undefined;
+            if (key == 'xmlns:tns' || key == 'xmlns:xsi' || key == 'xsi:schemaLocation' || key == 'xmlns:dbear' || key == 'comment') result = undefined;
             return result;
         }, 4)
 
