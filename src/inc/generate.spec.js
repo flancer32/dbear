459760ab393f7d4b6beec38da407a1c2d172fbc0
Viktor <viktor@nmmlm.com>
2015-07-20 19:21:54 +0300

    Code review

--- src/inc/generate.spec.js
+++ src/inc/generate.spec.js
@@ -1,24 +1,24 @@
 /* Define dependencies */
 //var request = require('../../sample/01_person/sample_01.dem.json');
-var Generator = require('./generate');
-var assert = require('chai');
-var params = require('./generate/params');
+var Generator = require('./generate')
+var assert = require('chai')
+var params = require('./generate/params')
 var sinon = require('sinon').sandbox.create()
-var Sequelize = require('sequelize');
+var Sequelize = require('sequelize')
 
 describe('Generator module', function () {
     describe('#setConnection()', function () {
-        var sg = new Generator();
+        var sg = new Generator()
 
         //var params = new Params;
-        params.dbLogin = 'sample';
-        params.dbName = 'sample_sequelize';
-        params.dbPass = '3Jcftix7VycNkEYKxIDW';
-        params.dbHost = 'localhost';
-        params.dbDialect = 'mysql';
+        params.dbLogin = 'sample'
+        params.dbName = 'sample_sequelize'
+        params.dbPass = '3Jcftix7VycNkEYKxIDW'
+        params.dbHost = 'localhost'
+        params.dbDialect = 'mysql'
         // 1st test
         it('should authenticate with correct data', function (done) {
-            //sg.sequelize = new Sequelize('sample', 'sample_sequelize', '3Jcftix7VycNkEYKxIDW');
+            console.log("Test01 START!")
 
             sinon.stub(sg, 'getOrm', function () {
                 return function (database, username, password, options) {
@@ -32,7 +32,7 @@ describe('Generator module', function () {
             })
 
             sg.setConnection(params).then(function (resolve) {
-                assert.isTrue(true, 'Connection test1 succeed');
+                assert.isTrue(true, 'Connection test1 succeed')
                 done()
             }, function (reject) {
                 assert.isTrue(false, 'Connection test1 failed \n' + reject)
