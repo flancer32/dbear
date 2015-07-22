52323f1a251e7b660050e6a0c83cb14c8f5a5e75
Alexandr-Baluhin <alexbalwork@gmail.com>
2015-07-22 18:16:57 +0300

    Merge Convertor.js and Convert.js

--- src/inc/convert.js
+++ src/inc/convert.js
@@ -1,11 +1,128 @@
 'use strict'
+
 var fs = require('fs') // fs instance
-var parseString = require('xml2js').parseString // xml2js instance
+var parseString = require('xml2js').parseString // xml2js method parseString instance
+var prefixMatch = new RegExp(/(?!xmlns)^.*:/);
 
-var prefixMatch = new RegExp(/(?!xmlns)^.*:/)
+/*
+ ---Bunch of functions to create a new valid json structure (relations part)
+ */
+function analyzeRelations(request) {
+    function analyzeRefs(request) {
 
+        var result = {};
 
+        if (request.hasOwnProperty('comment') || request.hasOwnProperty('namespace')) {
+            result.comment = request.comment;
+            result.namespace = request.namespace;
+        }
 
+        result.id = request.id;
+
+        if (request.hasOwnProperty('indexes')) {
+            result.indexes = [];
+            if (Array.isArray(request.indexes.index)) {
+                for (var i = 0; i < request.indexes.index.length; i++) {
+                    result.indexes[i] = request.indexes.index[i]
+                }
+            } else result.indexes[0] = request.indexes.index
+        }
+        return result;
+    }
+
+    var result = {};
+    result.id = request.id;
+    if (request.hasOwnProperty('comment')) {
+        result.comment = request.comment;
+    }
+    result.refs = [];
+    if (Array.isArray(request.refs.entryRef)) {
+        for (var i = 0; i < request.refs.entryRef.length; i++) {
+            result.refs[i] = analyzeRefs(request.refs.entryRef[i])
+        }
+    } else result.refs[0] = analyzeRefs(request.refs.entryRef)
+
+    return result;
+}
+
+/*
+ ---Bunch of functions to create a new valid json structure (main part)
+ */
+function analyze(request) {
+    function analyzeNamespaces(request) {
+        function analyzeEntities(request) {
+            function analyzeAttr(request) {
+
+                var result = {};
+                result.id = request.id;
+                if (request.hasOwnProperty('comment')) {
+                    result.comment = request.comment;
+                }
+                result.type = request.type;
+
+                return result;
+
+            }
+
+            var result = {};
+            result.id = request.id;
+            if (request.hasOwnProperty('comment')) {
+                result.comment = request.comment;
+            }
+            result.attributes = [];
+            if (Array.isArray(request.attributes.attribute)) {
+                for (var i = 0; i < request.attributes.attribute.length; i++) {
+                    result.attributes[i] = analyzeAttr(request.attributes.attribute[i])
+                }
+            } else result.attributes[0] = analyzeAttr(request.attributes.attribute)
+
+            return result;
+
+        }
+
+        var result = {};
+
+        result.id = request.id;
+        if (request.hasOwnProperty('comment')) {
+            result.comment = request.comment;
+        }
+        result.entities = [];
+        if (Array.isArray(request.entities.entity)) {
+            for (var i = 0; i < request.entities.entity.length; i++) {
+                result.entities[i] = analyzeEntities(request.entities.entity[i])
+            }
+        } else result.entities[0] = analyzeEntities(request.entities.entity)
+
+        if (request.hasOwnProperty('relations')) {
+            result.relations = [];
+            if (Array.isArray(request.relations.relation)) {
+                for (var i = 0; i < request.relations.relation.length; i++) {
+                    result.relations[i] = analyzeRelations(request.relations.relation[i])
+                }
+            } else result.relations[0] = analyzeRelations(request.relations.relation)
+        }
+
+        return result;
+    }
+
+    var result = {"dBEAR":{}};
+
+    if (request.dBEAR.hasOwnProperty('comment')) {
+        result.dBEAR.comment = request.dBEAR.comment;
+    }
+    result.dBEAR.namespaces = [];
+    if (Array.isArray(request.dBEAR.namespaces.namespace)) {
+        for (var i = 0; i < request.dBEAR.namespaces.namespace.length; i++) {
+            result.dBEAR.namespaces[i] = analyzeNamespaces(request.dBEAR.namespaces.namespace[i])
+        }
+    } else result.dBEAR.namespaces[0] = analyzeNamespaces(request.dBEAR.namespaces.namespace)
+
+    return result
+}
+
+/*
+ ---Main part of convertor
+ */
 function Converter() {
     this.run = function (param) {
         var fileIn = param.demFileIn
@@ -15,67 +132,19 @@ function Converter() {
             parseString(data, {
                     tagNameProcessors: [tagStripPrefix], // strip tag prefix
                     explicitArray: false, // remove arrays in child nodes
-                    mergeAttrs: true
-                }, // attributes become child nodes
+                    mergeAttrs: true, // attributes become child nodes
+                    emptyTag: {}}, // default value of empty tag
 
-                function (err, result) {
-
-                    function analyze(request) {
-                        function analyzeNamespaces(request) {
-                            function analyzeEntities(request) {
-                                function analyzeAttr(request) {
-                                    var result = {};
-                                    result.field = request.id;
-                                    for (var i = 0; i < request.attributes.length; i++) {
-                                        result.attributes[i] = request.attributes[i]
-                                    }
-                                    return result
-                                }
-
-                                var result = {}
-                                result.id = request.id
-                                if (request.hasOwnProperty('comment')) {
-                                    result.comment = request.comment
-                                }
-                                result.attributes = []
-                                for (var i = 0; i < request.attributes.length; i++) {
-                                    result.attributes[i] = analyzeAttr(request.attributes[i])
-                                }
-                                return result
-                            }
-
-                            var result = {}
-                            result.id = request.id
-                            if (request.hasOwnProperty('comment')) {
-                                result.comment = request.comment
-                            }
-                            result.entities = []
-                            for (var i = 0; i < request.entities.length; i++) {
-                                result.entities[i] = analyzeEntities(request.entities[i])
-                            }
-                            return result
-                        }
-
-                        var result = {};
-                        if (request.dBEAR.hasOwnProperty('comment')) {
-                            result.comment = request.dBEAR.comment
-                        }
-                        result.namespaces = []
-                        for (var i = 0; i < request.dBEAR.namespaces.length; i++) {
-                            result.dbear.namespaces[i] = analyzeNamespaces(request.dBEAR.namespaces[i])
-                        }
-                        return result
-                    }
-
-                    /* TODO this function works unproperly. Rewrite it.
-                     * #Created on 21-Jul-15
-                     * */
-                    var common = analyze(result) // common is a new result
-                    var str = JSON.stringify(common, function (key, value) {
+                function (err, result) // callable function
+                {
+                    var common = analyze(result) // get a new json structure
+                    var str = JSON.stringify(common,
+                        function (key, value) // callable function to strip some useless nodes
+                        {
                         var result = value
-                        if (key == 'xmlns:tns' || key == 'xmlns:xsi' || key == 'xsi:schemaLocation') result = undefined
-                        return result
-                    }, 4)
+                        if (key == 'xmlns:tns' || key == 'xmlns:xsi' || key == 'xsi:schemaLocation') result = undefined;
+                        return result;
+                    }, 2)
                     /* ded.json is writing to the same directory. Maybe we should create special folder? */
                     fs.writeFile(fileOut, str)
                 })
@@ -83,7 +152,9 @@ function Converter() {
     }
 }
 
-
+/*
+---Function to strip tag prefix
+*/
 function tagStripPrefix(name) {
     /* Is this function obliged? It is used once. */
     var result = name.replace(prefixMatch, '')
